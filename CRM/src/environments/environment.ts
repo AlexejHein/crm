70b03490ab3c671e0c660efe77942664a1f7fb67
AlexejHein <134226688+AlexejHein@users.noreply.github.com>
2023-11-13 22:03:29 +0100

    add menu

---
+++ CRM/src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBaIOhQzPMNXltEGJnFuITnbuvZX3IA81Q",
+    authDomain: "crm-3f8c1.firebaseapp.com",
+    projectId: "crm-3f8c1",
+    storageBucket: "crm-3f8c1.appspot.com",
+    messagingSenderId: "785843487108",
+    appId: "1:785843487108:web:7e87e7970884191d749dd1"
+  }
+};
