291f8e03d919e39093b1fb8dcef621e1d94b0713
AlexejHein <134226688+AlexejHein@users.noreply.github.com>
2023-11-24 18:29:30 +0100

    fix login

--- CRM/src/app/login/login.component.ts
+++ CRM/src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import  { Router } from "@angular/router";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-login',
@@ -8,23 +9,43 @@ import  { Router } from "@angular/router";
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  loginForm: FormGroup;
+  loginError: string = '';
   hide = true;
   email: string | undefined;
   password: string | undefined;
 
-  constructor(private authService: AuthService, private router: Router) { }
+  constructor(private authService: AuthService, private router: Router, private fb: FormBuilder,) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
 
   login() {
-    this.authService.login(this.email, this.password)
-      .then(res => {
-        this.router.navigate(['/dashboard']).then(r => console.log(r));
-        console.log('Erfolgreich eingelogt!', res);
-      })
-      .catch(err => {
-        this.router.navigate(['/']).then(r => console.log(r));
-        console.log('Etwas ist schief gelaufen:', err.message);
-      });
+    if (this.loginForm.valid) {
+      this.authService.login(this.loginForm.value.email, this.loginForm.value.password)
+        .then(res => {
+          this.router.navigate(['/dashboard']).then(r => console.log('Login successful'));
+        })
+        .catch(err => {
+          if (err.code === 'auth/user-not-found') {
+            this.loginError = 'User does not exist.';
+          } else {
+            this.loginError = 'An error occurred during login.';
+          }
+        });
+    } else {
+      this.loginError = 'Please correct the errors in the form.';
+    }
+  }
 
+
+  guestLogin() {
+    this.loginForm.setValue({
+      email: 'guest@login.com',
+      password: '34cv42rttcf5tz3t4'
+    });
   }
 
 }
