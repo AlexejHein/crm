20d9a8851244083eef5cbddfea97ea575ca65787
AlexejHein <134226688+AlexejHein@users.noreply.github.com>
2023-12-13 09:56:28 +0100

    add form validators add task

--- CRM/src/app/task/add-task/add-task.component.ts
+++ CRM/src/app/task/add-task/add-task.component.ts
@@ -3,6 +3,7 @@ import { Task} from "../../models/tasks.class";
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { MatDialogRef } from "@angular/material/dialog";
 import { Customer } from '../../models/customers.class';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-task',
@@ -10,11 +11,20 @@ import { Customer } from '../../models/customers.class';
   styleUrls: ['./add-task.component.scss']
 })
 export class AddTaskComponent {
+  taskForm: FormGroup;
   customer = new Customer();
   allCustomers: any[] = [];
   task = new Task();
   loading = false;
-  constructor(private firestore: AngularFirestore, public dialogRef: MatDialogRef<AddTaskComponent>) { }
+  constructor(private firestore: AngularFirestore, public dialogRef: MatDialogRef<AddTaskComponent>) {
+    this.taskForm = new FormGroup({
+      'title': new FormControl(null, Validators.required),
+      'description': new FormControl(null, Validators.required),
+      'priority': new FormControl(null, Validators.required),
+      'assignedTo': new FormControl(null, Validators.required),
+      'status': new FormControl(null, Validators.required)
+    });
+  }
 
   ngOnInit(): void {
     this.firestore.collection('customers')
@@ -24,11 +34,13 @@ export class AddTaskComponent {
       });
   }
   saveTask() {
-    this.loading = true;
-    this.firestore.collection('tasks').add(this.task.toJSON())
-      .then(r => {
-        this.loading = false;
-      });
-    this.dialogRef.close();
+    if (this.taskForm.valid) {
+      this.loading = true;
+      this.firestore.collection('tasks').add(this.taskForm.value)
+        .then(r => {
+          this.loading = false;
+        });
+      this.dialogRef.close();
+    }
   }
 }
