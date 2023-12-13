8a76650e0841b80200369ef112d75f8dce867f72
AlexejHein <134226688+AlexejHein@users.noreply.github.com>
2023-12-13 09:24:21 +0100

    add form validators save Products

--- CRM/src/app/sales/add-product/add-product.component.ts
+++ CRM/src/app/sales/add-product/add-product.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { MatDialogRef } from "@angular/material/dialog";
 import { Products } from "../../models/products.class";
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-add-product',
@@ -9,15 +11,24 @@ import { Products } from "../../models/products.class";
   styleUrls: ['./add-product.component.scss']
 })
 export class AddProductComponent {
+    productForm: FormGroup;
     product = new Products();
     loading = false;
-    constructor(private firestore: AngularFirestore, public dialogRef: MatDialogRef<AddProductComponent>) { }
-    saveProduct() {
+  constructor(private firestore: AngularFirestore, public dialogRef: MatDialogRef<AddProductComponent>) {
+    this.productForm = new FormGroup({
+      'name': new FormControl(null, Validators.required),
+      'description': new FormControl(null, Validators.required),
+      'price': new FormControl(null, [Validators.required, Validators.min(0)])
+    });
+  }
+  saveProduct() {
+    if (this.productForm.valid) {
       this.loading = true;
-      this.firestore.collection('products').add(this.product.toJSON())
+      this.firestore.collection('products').add(this.productForm.value)
         .then(r => {
           this.loading = false;
         });
       this.dialogRef.close();
     }
+  }
 }
